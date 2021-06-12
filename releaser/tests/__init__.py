969293813b07b4464bc92d2a25abe8e00b3217b3
Jelmer Vernooĳ <jelmer@jelmer.uk>
2021-06-12 12:22:55 +0100

    Add tests

---
+++ releaser/tests/__init__.py
@@ -0,0 +1,29 @@
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; version 2
+# of the License or (at your option) any later version of
+# the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Tests for releaser."""
+
+import unittest
+
+
+def test_suite():
+    names = [
+        ]
+    module_names = ['releaser.tests.' + name for name in names]
+    result = unittest.TestSuite()
+    loader = unittest.TestLoader()
+    suite = loader.loadTestsFromNames(module_names)
+    result.addTests(suite)
+
+    return result
