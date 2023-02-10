c65df4bbe07148bc4c32c72b634958c9671dcdc5
Jelmer Vernooĳ <jelmer@jelmer.uk>
2023-02-10 17:49:19 +0000

    Release 0.0.6.

--- disperse/__init__.py
+++ disperse/__init__.py
@@ -16,7 +16,7 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
 
-__version__ = (0, 0, 5)
+__version__ = (0, 0, 6)
 version_string = '.'.join(map(str, __version__))
 
 
